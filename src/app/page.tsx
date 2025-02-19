f4777ee88d35ac94d48705a7b477ccd71f9a21ac
Jakub <jakub@sidorowi.cz>
2025-02-19 15:34:32 +0100

    add skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
-import { EXPERIENCE } from "@/data/experience";
+import { EXPERIENCE, SKILLS } from "@/data/experience";
 
 export default function Home() {
     return (
@@ -25,22 +25,40 @@ export default function Home() {
                             <div className="leading-7">
                                 <p>
                                     I'm a passionate and results-driven
-                                    developer, living in Vienna, Austria. I
-                                    began my{" "}
-                                    <Link href="/experience">career</Link> in
-                                    advertising agencies, where I established
-                                    myself as a passionate and dedicated Web
-                                    developer. After few years my career led me
-                                    into the start-up scene. With a strong
-                                    motivation to solve real-world problems — I
-                                    specialize in building scalable web
-                                    applications for high traffic.
+                                    developer, living in Vienna,{" "}
+                                    <a
+                                        href="https://www.austria.info/"
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                        aria-label="Repository of this page (new tab)"
+                                    >
+                                        Austria
+                                    </a>
+                                    . I began my career in advertising agencies,
+                                    where I established myself as a passionate
+                                    and dedicated Web developer. After few years
+                                    my{" "}
+                                    <Link href="/experience">
+                                        developer career
+                                    </Link>{" "}
+                                    led me into the start-up scene. With a
+                                    strong motivation to solve real-world
+                                    problems — I specialize in building scalable
+                                    web applications for high traffic.
                                 </p>
                                 <p>
-                                    A strong advocate for agile methodologies, I
-                                    thrive in fast-paced, collaborative
-                                    environments and continuously seek
-                                    innovative and efficient solutions. A
+                                    A strong advocate for{" "}
+                                    <a
+                                        href="https://www.agilealliance.org/agile101/"
+                                        target="_blank"
+                                        rel="noreferrer noopener"
+                                        aria-label="What is Agile? (new tab)"
+                                    >
+                                        {SKILLS.methodologies.agile}
+                                    </a>{" "}
+                                    methodologies, I thrive in fast-paced,
+                                    collaborative environments and continuously
+                                    seek innovative and efficient solutions. A
                                     regular attendee of developer conferences —
                                     always exploring new technologies. Iny my
                                     free time I enjoy climbing, skiing and other
@@ -52,7 +70,7 @@ export default function Home() {
                             <h3>Experience</h3>
                             <ol>
                                 {EXPERIENCE.map((exp, index) => (
-                                    <li key={index} className="mb-6">
+                                    <li key={index} className="mb-8">
                                         <h4 className="font-semibold">
                                             {exp.company}
                                         </h4>
@@ -60,8 +78,18 @@ export default function Home() {
                                             {exp.role}
                                         </div>
                                         <div>{exp.duration}</div>
-                                        <div className="mt-2">
+                                        <div className="mt-1">
                                             {exp.description}
+                                        </div>
+                                        <div className="space-x-2 mt-2.5">
+                                            {exp.skills.map((skill, index) => (
+                                                <span
+                                                    key={index}
+                                                    className="bg-pal-green text-pal-white text-xs font-medium px-2 py-1 rounded-full"
+                                                >
+                                                    {skill}
+                                                </span>
+                                            ))}
                                         </div>
                                     </li>
                                 ))}
@@ -86,7 +114,7 @@ export default function Home() {
                             rel="noreferrer noopener"
                             aria-label="Next.js (new tab)"
                         >
-                            Next.js
+                            {SKILLS.frontend.next}
                         </a>
                         ,{" "}
                         <a
@@ -95,10 +123,10 @@ export default function Home() {
                             rel="noreferrer noopener"
                             aria-label="Tailwind CSS (new tab)"
                         >
-                            Tailwind CSS
+                            {SKILLS.frontend.tailwind}
                         </a>{" "}
-                        and Sass. Deployed on Vercel. Feel free to check the
-                        public{" "}
+                        and {SKILLS.frontend.sass}. Deployed on{" "}
+                        {SKILLS.tools.vercel}. Feel free to check the public{" "}
                         <a
                             href="https://github.com/sidor/personal"
                             target="_blank"
--- src/data/experience.ts
+++ src/data/experience.ts
@@ -1,10 +1,59 @@
+export const SKILLS = {
+    frontend: {
+        javascript: "JavaScript",
+        typescript: "TypeScript",
+        react: "React",
+        vue: "Vue.js",
+        next: "Next.js",
+        gatsby: "Gatsby",
+        tailwind: "Tailwind CSS",
+        sass: "Sass",
+        styledComponents: "Styled Components",
+        seo: "SEO Optimization",
+        storyBook: "Storybook",
+    },
+    backend: {
+        node: "Node.js",
+        laravel: "Laravel",
+        php: "PHP",
+        graphql: "GraphQL",
+        mongo: "MongoDB",
+        mysql: "MySQL",
+    },
+    testing: {
+        jest: "Jest",
+        mocha: "Mocha",
+        karma: "Karma",
+    },
+    tools: {
+        jira: "Jira",
+        figma: "Figma",
+        firebase: "Firebase",
+        vercel: "Vercel",
+        github: "GitHub",
+        bitbucket: "Bitbucket",
+    },
+    methodologies: {
+        agile: "Agile",
+        kanban: "Kanban",
+        scrum: "Scrum",
+    },
+};
+
 export const EXPERIENCE = [
     {
         company: "Self-Employed",
         role: "Gun for hire",
         duration: "2023 - Present",
         description:
-            "Supporting Viennese start-ups by creating great web applications. Key projects — Kavo, Sneakit.",
+            "Supporting start-ups by creating great web applications. Key projects — Kavo, Sneakit.",
+        skills: [
+            SKILLS.frontend.react,
+            SKILLS.frontend.vue,
+            SKILLS.frontend.tailwind,
+            SKILLS.backend.node,
+            SKILLS.backend.laravel,
+        ],
     },
     {
         company: "Miracl GmbH",
@@ -12,6 +61,14 @@ export const EXPERIENCE = [
         duration: "2021 - 2023",
         description:
             "Led the development of a product website and survey with mortgage comparison tool, which presented financing options from multiple banks. Focused on optimizing user engagement, iterating the user flow to encourage data input for a seamless loan process.",
+        skills: [
+            SKILLS.frontend.react,
+            SKILLS.frontend.gatsby,
+            SKILLS.frontend.styledComponents,
+            SKILLS.backend.graphql,
+            SKILLS.backend.node,
+            SKILLS.tools.firebase,
+        ],
     },
     {
         company: "TREAM Limited",
@@ -19,5 +76,12 @@ export const EXPERIENCE = [
         duration: "2018 - 2021",
         description:
             "Developed a subscription-based fitness video platform with shared income for trainers from around the world.",
+        skills: [
+            SKILLS.frontend.next,
+            SKILLS.frontend.styledComponents,
+            SKILLS.frontend.storyBook,
+            SKILLS.backend.mongo,
+            SKILLS.backend.node,
+        ],
     },
 ];
