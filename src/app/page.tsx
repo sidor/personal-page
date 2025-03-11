6ecb592a96a1c58ac9118ff48802e06cc6088f72
Jakub <jakub@sidorowi.cz>
2025-03-11 11:12:45 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,130 +1,68 @@
+import Image from "next/image";
 import Link from "next/link";
 import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
-import { EXPERIENCE, SKILLS } from "@/data/experience";
 
 export default function Home() {
     return (
         <div className="flex flex-col min-h-screen">
-            <div className="container pt-14">
-                <header className="side">
-                    <h1 className="text-pal-brown">
-                        <Link
-                            href="/"
-                            title="Go to homepage"
-                            aria-label="Go to homepage"
-                        >
-                            Jakub Sidorowicz
-                        </Link>
-                    </h1>
-                    <h2 className="bitter font-medium text-xl text-whote mt-2">
-                        Software Engineer
-                    </h2>
-                    <div className="media flex flex-row gap-4 mt-6 md:mt-12 text-xl">
-                        <a
-                            href="https://www.linkedin.com/in/sidorowicz/"
-                            target="_blank"
-                            rel="noreferrer noopener"
-                            aria-label="Opens linkedin profile (new tab)"
-                        >
-                            <FaLinkedin />
-                        </a>
-                        <a
-                            href="https://github.com/sidor/"
-                            target="_blank"
-                            rel="noreferrer noopener"
-                            aria-label="Opens github user profile (new tab)"
-                        >
-                            <FaGithub />
-                        </a>
-                        <a
-                            href="mailto:jakub@sidorowi.cz"
-                            rel="noreferrer noopener"
-                            aria-label="Opens your email client"
-                        >
-                            <FaEnvelope />
-                        </a>
-                    </div>
-                </header>
-                <main className="main">
-                    <section>
-                        <h3>About me</h3>
-                        <p>
-                            I'm a passionate and results-driven developer,
-                            living in Vienna,{" "}
-                            <a
-                                href="https://www.austria.info/"
-                                target="_blank"
-                                rel="noreferrer noopener"
-                                aria-label="Repository of this page (new tab)"
-                            >
-                                Austria
-                            </a>
-                            . I began my career in advertising agencies, where I
-                            established myself as a passionate and dedicated Web
-                            developer. After few years my{" "}
-                            <Link href="/experience">developer career</Link> led
-                            me into the start-up scene. With a strong motivation
-                            to solve real-world problems — I specialize in
-                            building scalable web applications for high traffic.
-                        </p>
-                        <p>
-                            A strong advocate for{" "}
-                            <a
-                                href="https://www.agilealliance.org/agile101/"
-                                target="_blank"
-                                rel="noreferrer noopener"
-                                aria-label="What is Agile? (new tab)"
-                            >
-                                {SKILLS.methodologies.agile}
-                            </a>{" "}
-                            methodologies, I thrive in fast-paced, collaborative
-                            environments and continuously seek innovative and
-                            efficient solutions. A regular attendee of developer
-                            conferences — always exploring new technologies. Iny
-                            my free time I enjoy climbing, skiing and other
-                            sports like yoga or running.
-                        </p>
-                    </section>
-                    <section>
-                        <h3>Experience</h3>
-                        <ol className="timeline">
-                            {EXPERIENCE.map((exp, index) => (
-                                <li key={index} className="mb-8">
-                                    <h4 className="font-semibold">
-                                        {exp.company}
-                                    </h4>
-                                    {exp.role && (
-                                        <div className="text-pal-blue text-sm">
-                                            {exp.role}
-                                        </div>
-                                    )}
-                                    <div className="timeline__duration text-sm my-1 text-[#2d9e97] font-semibold">
-                                        {exp.duration}
-                                    </div>
-                                    <p className="mt-0">{exp.description}</p>
-                                    <div className="flex flex-wrap gap-2 mt-2.5">
-                                        {exp.skills.map((skill, index) => (
-                                            <span
-                                                key={index}
-                                                className="inline-flex bg-pal-green text-pal-white text-xs font-medium px-2 py-1 rounded-full whitespace-nowrap"
-                                            >
-                                                {skill}
-                                            </span>
-                                        ))}
-                                    </div>
-                                </li>
-                            ))}
-                        </ol>
-                        {/*<Link href="/experience">
-                            View all experience &raquo;
-                        </Link>*/}
-                    </section>
-                </main>
-            </div>
+            <div className="container flex flex-col">
+                <div className="flex justify-between gap-12 pt-14">
+                    <header className="">
+                        <h1 className="text-pal-brown">Jakub Sidorowicz</h1>
 
-            <div className="container container--footer md:flex-row-reverse md:gap-24 mt-auto">
-                <main className="main">
-                    <div className="md:max-w-md opacity-60 text-sm pb-2 xl:pb-3 leading-5">
+                        <p className="bitter font-medium text-xl text-whote mt-2">
+                            Software Engineer
+                        </p>
+                        <div className="flex flex-row gap-4 mt-12 text-pal-blue text-xl">
+                            <FaLinkedin />
+                            <FaGithub />
+                            <FaEnvelope />
+                        </div>
+                    </header>
+                    <div className="max-w-3xl">
+                        <section>
+                            <h3>About me</h3>
+                            <div className="leading-7">
+                                <p>
+                                    I&apos;mm a passionate and results-driven
+                                    developer, living in Vienna, Austria. I
+                                    began my{" "}
+                                    <Link href="/experience">career</Link> in
+                                    advertising agencies, where I established
+                                    myself as a passionate and dedicated Web
+                                    developer. After few years my career led me
+                                    into the start-up scene. With a strong
+                                    motivation to solve real-world problems — I
+                                    specialize in building scalable web
+                                    applications for high traffic.
+                                </p>
+                                <p>
+                                    A strong advocate for agile methodologies, I
+                                    thrive in fast-paced, collaborative
+                                    environments and continuously seek
+                                    innovative and efficient solutions. A
+                                    regular attendee of developer conferences —
+                                    always exploring new technologies. Iny my
+                                    free time I enjoy climbing, skiing and other
+                                    sports like yoga or running.
+                                </p>
+                            </div>
+                        </section>
+                        <section>
+                            <h3>Experience</h3>
+                            <ol>
+                                <li></li>
+                            </ol>
+                        </section>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-auto flex justify-center items-end overflow-hidden">
+                <div className="w-1/2 flex items-end justify-end">
+                    <div className="city"></div>
+                </div>
+                <div className="w-1/2 text-pal-white text-sm pl-7 pb-4 opacity-70">
+                    <div className="max-w-md">
                         Made with love in Vienna using{" "}
                         <a
                             href="https://nextjs.org/"
@@ -132,7 +70,7 @@ export default function Home() {
                             rel="noreferrer noopener"
                             aria-label="Next.js (new tab)"
                         >
-                            {SKILLS.frontend.next}
+                            Next.js
                         </a>
                         ,{" "}
                         <a
@@ -141,20 +79,10 @@ export default function Home() {
                             rel="noreferrer noopener"
                             aria-label="Tailwind CSS (new tab)"
                         >
-                            {SKILLS.frontend.tailwind}
+                            Tailwind CSS
                         </a>{" "}
-                        and{" "}
-                        <a
-                            href="https://sass-lang.com/"
-                            target="_blank"
-                            rel="noreferrer noopener"
-                            aria-label="Sass CSS extension (new tab)"
-                        >
-                            {SKILLS.frontend.sass}
-                        </a>
-                        . Deployed on {SKILLS.tools.vercel}.
-                        {/*Feel free to use
-                        the public{" "}
+                        and Sass. Deployed on Vercel. Feel free to check the
+                        public{" "}
                         <a
                             href="https://github.com/sidor/personal"
                             target="_blank"
@@ -163,15 +91,13 @@ export default function Home() {
                         >
                             repository
                         </a>{" "}
-                        on Github.*/}
-                    </div>
-                </main>
-                <div className="side relative">
-                    <div className="overflow-hidden w-full">
-                        <div className="city"></div>
+                        on Github.
                     </div>
                 </div>
             </div>
+            {/* <footer className="bg-pal-white text-pal-darkblue py-4">
+                <div className="container">&copy; 2025 Jakub Sidorowicz</div>
+            </footer> */}
         </div>
     );
 }
