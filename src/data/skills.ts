22ba13f96c695b11533d40b15a290967508e970d
Jakub <jakub@sidorowi.cz>
2025-04-02 12:47:57 +0200

    update experience

---
+++ src/data/skills.ts
@@ -0,0 +1,44 @@
+const SKILLS = {
+    frontend: {
+        javascript: "JavaScript",
+        typescript: "TypeScript",
+        react: "React",
+        vue: "Vue.js",
+        next: "Next.js",
+        gatsby: "Gatsby",
+        tailwind: "Tailwind CSS",
+        sass: "Sass",
+        styledComponents: "Styled Components",
+        seo: "SEO Optimization",
+        storyBook: "Storybook",
+    },
+    backend: {
+        node: "Node.js",
+        laravel: "Laravel",
+        php: "PHP",
+        graphql: "GraphQL",
+        mongo: "MongoDB",
+        mysql: "MySQL",
+    },
+    testing: {
+        jest: "Jest",
+        mocha: "Mocha",
+        karma: "Karma",
+    },
+    tools: {
+        jira: "Jira",
+        figma: "Figma",
+        firebase: "Firebase",
+        vercel: "Vercel",
+        github: "GitHub",
+        bitbucket: "Bitbucket",
+        stripe: "Stripe",
+    },
+    methodologies: {
+        agile: "Agile",
+        kanban: "Kanban",
+        scrum: "Scrum",
+    },
+};
+
+export default SKILLS;
